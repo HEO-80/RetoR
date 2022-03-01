16d19ab8d1531f344377c3af7063e0b9e589448b
Hector Oviedo <eovertigo@gmail.com>
2022-03-01 10:53:42 +0100

    Realizado ejercicio 4 permite editar un pedido añadiendo o eliminando productos.

--- RetoBackend/src/main/java/com/example/recuperacion/Controllers/ProductosController.java
+++ RetoBackend/src/main/java/com/example/recuperacion/Controllers/ProductosController.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.example.recuperacion.ElementNotFoundException;
 import com.example.recuperacion.Models.Productos;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,4 +40,75 @@ public class ProductosController {
         public List<Productos> GetAllProductos() {
                 return productos;
         }
+
+        @GetMapping("Productos/{NombreProducto}")
+        public static Productos ProductoByNombre(@PathVariable("NombreProducto") String NombreProducto) {
+                return FindProductoByNombreProducto(NombreProducto);
+        }
+
+        public static Productos FindProductoByNombreProducto(String NombreProducto) {
+                for (Productos producto : productos) {
+                        if (producto.getNombreProducto() == NombreProducto) {
+                                return producto;
+                        }
+                }
+                throw new ElementNotFoundException();
+        }
+
+        public static Productos FindProductoByPrecio(double precio) {
+                for (Productos producto : productos) {
+                        if (producto.getPrecio() == precio) {
+                                return producto;
+                        }
+                }
+                throw new ElementNotFoundException();
+        }
+
+        public static Productos FindProductoByIdProducto(int idProducto) {
+                for (Productos producto : productos) {
+                        if (producto.getIdProducto() == idProducto) {
+                                return producto;
+                        }
+                }
+                throw new ElementNotFoundException();
+        }
+
+        public static ArrayList<Productos> GetProductosByPedidoId(int idPedido) {
+                ArrayList<Productos> result = productos;
+                if (idPedido != 0) {
+                        result = new ArrayList<Productos>();
+                        for (Productos producto : productos) {
+                                if (producto.IdProducto == idPedido) {
+                                        result.add(producto);
+                                }
+                        }
+
+                }
+                return result;
+        }
+
+        @PostMapping("/add/Productos")
+        public Productos AddProducto(@RequestBody Productos newProducto) {
+                productos.add(newProducto);
+                return newProducto;
+        }
+
+        // @PutMapping("/Productos/{idProducto}")
+        @PutMapping("/Productos/{idProducto}")
+        public Productos updateProductos(@PathVariable("idProducto") int idProducto,
+                        @RequestBody Productos updateProducto)
+                        throws Exception {
+                Productos m = FindProductoByIdProducto(idProducto);
+                m.setPrecio(updateProducto.getPrecio());
+                return m;
+        }
+
+        // @DeleteMapping("/Productos/{idProducto}")
+        @DeleteMapping("/Productos/{idProducto}")
+        public Productos DeleteProducto(@PathVariable("idProducto") int idProducto) {
+                Productos producto = FindProductoByIdProducto(idProducto);
+                productos.remove(idProducto);
+                return producto;
+        }
+
 }
